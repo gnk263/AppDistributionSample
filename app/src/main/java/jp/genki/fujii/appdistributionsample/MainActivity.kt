cdbc1eaedcc77b6a187394c77ef5c3f3dbb8b268
gnk263 <gnk263@users.noreply.github.com>
2019-11-04 21:10:26 +0900

    add android project

---
+++ app/src/main/java/jp/genki/fujii/appdistributionsample/MainActivity.kt
@@ -0,0 +1,12 @@
+package jp.genki.fujii.appdistributionsample
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
